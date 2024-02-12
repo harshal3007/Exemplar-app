d0466318cb604298930d619f13d7b4d889d3f928
harshal <harshalbhawsar9@gmail.com>
2024-02-12 18:58:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Example from './Component/Hero';
+
+function App() {
+  return (
+    <div>
+      <Example/>
+    </div>
+  );
+}
+
+export default App;
