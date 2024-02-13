20e0ca30e067a2afa1fa247e9c228b739f734419
harshal <harshalbhawsar9@gmail.com>
2024-02-13 19:38:12 +0530

    Slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Example from './Component/Hero';
+import { Carousel5 } from './Component/PartnerSlider';
 
 function App() {
   return (
     <div>
       <Example/>
+      <Carousel5/>
     </div>
   );
 }
