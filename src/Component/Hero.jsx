22d9cc8757f5f4ad7c131ee6e1e852802feb91c7
harshal <harshalbhawsar9@gmail.com>
2024-02-19 11:37:15 +0530

    slider fixed and call popup

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -14,7 +14,7 @@ const navigation = [
 
 const Home = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
+  const [isModalOpen, setModalOpen] = useState(false);
   return (
     <div className="bg-white">
       <header className="absolute inset-x-0 top-0 z-50">
@@ -118,6 +118,7 @@ const Home = () => {
                   </Link>
                   <div
                     className="text-sm font-semibold leading-6 text-gray-900"
+                    onClick={() => setModalOpen(true)}
                   >
                     Book A Call{" "}
                     <span aria-hidden="true" className="text-baseorange">
@@ -137,6 +138,49 @@ const Home = () => {
           />
         </div>
       </div>
+
+{isModalOpen && (
+  <div id="crud-modal" tabindex="-1" aria-hidden="true" class="overflow-y-auto overflow-x-hidden fixed inset-x-1/4 top-36 z-50 justify-center items-center w-full inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-2xl max-h-full">
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                    Book a call
+                </h3>
+                <button onClick={() => setModalOpen(false)} type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <form class="p-4 md:p-5">
+                <div class="grid gap-4 mb-4 grid-cols-2">
+                    <div class="col-span-2">
+                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type name" required="" />
+                    </div>
+                    <div class="col-span-2 sm:col-span-1">
+                        <label for="number" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Number</label>
+                        <input type="number" name="number" id="number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type phone number" required="" />
+                    </div>
+                    <div class="col-span-2 sm:col-span-1">
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type phone email" required="" />
+                    </div>
+                    <div class="col-span-2">
+                        <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Note</label>
+                        <textarea id="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-baseorange focus:border-baseorange dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-baseorange dark:focus:border-baseorange" placeholder="Leave a note"></textarea>                    
+                    </div>
+                </div>
+                <button type="submit" class="text-white inline-flex items-center bg-baseorange hover:bg-custom focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-baseorange dark:hover:bg-baseorange dark:focus:ring-baseorange">
+                    Book now
+                </button>
+            </form>
+        </div>
+    </div>
+</div> 
+)}
     </div>
   );
 };
